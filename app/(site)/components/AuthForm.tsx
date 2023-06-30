500328acbbdb9f8a7c416658a909fb7dfb51f316
Hwang Seok O <ohs6006@gmail.com>
2023-06-30 14:34:42 +0900

    Auth UI

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,189 @@
 "use client";
 
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/Input";
+import { useCallback, useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+
+type Variant = "LOGIN" | "REGISTER";
+
 const AuthForm = () => {
-  return <div>Auth Form!</div>;
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = data => {
+    setIsLoading(true);
+    if (variant === "REGISTER") {
+      // 서버에 가입 요청
+    }
+
+    if (variant === "LOGIN") {
+      // NextAuth에 로그인 요청
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+    // NextAuth에 소셜 로그인 요청
+  };
+
+  return (
+    <div
+      className="
+   mt-8
+   sm:mx-auto
+   sm:w-full
+   sm:max-w-md
+  "
+    >
+      <div
+        className="
+      bg-white
+      px-4
+      py-8
+      shadow
+      sm:rounded-lg
+      sm:px-10
+      "
+      >
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input
+              id="name"
+              label="Name"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            id="email"
+            label="Email"
+            type="email"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id="paaword"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Button disabled={isLoading} type="submit" fullWidth>
+            {variant === "LOGIN" ? "로그인" : "회원 가입"}
+          </Button>
+        </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+                  absolute
+                  inset-0
+                  flex
+                  items-center
+                "
+            >
+              <div
+                className="
+                    w-full 
+                    border-t 
+                    border-gray-300
+                    "
+              />
+            </div>
+            <div
+              className="
+                  relative 
+                  flex 
+                  justify-center 
+                  text-sm
+                "
+            >
+              <span
+                className="
+                  bg-white 
+                  px-2 
+                  text-gray-500
+                  "
+              >
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+        <div
+          className="
+            mt-6
+            flex
+            justify-center
+            gap-2
+            px-2
+            text-sm
+            text-gray-500
+          "
+        >
+          <div>
+            {variant === "LOGIN"
+              ? "새 계정이 필요하신가요?"
+              : "이미 계정이 있으신가요?"}
+          </div>
+          <div onClick={toggleVariant} className="cursor-pointer underline">
+            {variant === "LOGIN" ? "새 계정 만들기" : "로그인 하기"}
+          </div>
+        </div>
+
+        <div
+          className="
+            mt-6
+            flex
+            justify-center
+            gap-2
+            px-2
+            text-sm
+            text-gray-500
+          "
+        >
+          <div>상담 센터 직원 이신가요?</div>
+          <div className="cursor-pointer underline">직원 회원 가입 하기</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
